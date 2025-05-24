cd778b8741e63b01e1e87b1c0cda3f5e910475c7
Soumya Nasipuri <nasipurisoumya@gmail.com>
2025-05-25 03:01:59 +0530

    :lipstick: style: react query and mantine has been installed

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 import Router from './Router';
+import { Notifications } from "@mantine/notifications";
+import { MantineProvider, ModalContent } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
 
 const App = () => {
     return (
-        <Router /> // This is the main entry point for the application, rendering the Router component
+        <MantineProvider defaultColorScheme='dark' >
+            <ModalsProvider>
+                <Router />
+                <Notifications position="top-right" />
+            </ModalsProvider>
+        </MantineProvider>
     )
 };
 export default App;
